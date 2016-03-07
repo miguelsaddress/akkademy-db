86b178d955e93e30055cd8ba2084029001dd68de
Miguel Angel Moreno <miguel.s.address@gmail.com>
2016-03-07 07:58:20 +0100

    Main for a server, to stay listening

--- src/main/scala/com/akkademy/Main.scala
+++ src/main/scala/com/akkademy/Main.scala
@@ -2,18 +2,10 @@ import akka.actor.Actor
 import akka.actor.Props
 import akka.actor.ActorSystem
 import akka.actor.ActorLogging
-
-import com.akkademy.messages._
 import com.akkademy.AkkademyDb
 
-object Main extends App {
-    val system = ActorSystem("akkademy-db")
-    val akkademyDb = system.actorOf(Props[AkkademyDb], "akkademy-db")
 
-    akkademyDb ! SetRequest("Hola", "Buna")
-    akkademyDb ! SetRequest("Adiós", "La revedere")
-    akkademyDb ! SetRequest("Gracias", "Multumesc")
-    
-    //system.awaitTermination()
-    system.shutdown()
+object Main extends App {
+    val system = ActorSystem("akkademy")
+    system.actorOf(Props[AkkademyDb], name = "akkademy-db")
 }
